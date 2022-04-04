ce202aa2109e648608421976dde0bae48aa1f456
BrunoBerger <bruno.berger@stud.hshl.de>
2022-04-04 21:58:39 +0200

    fix top level cmake to build in the sub-folders

--- u01basics/exec/src/matrixMath.cpp
+++ u01basics/exec/src/matrixMath.cpp
@@ -6,7 +6,7 @@
 arma::mat getRandomMat(int h, int w)
 {
     arma::mat matr(h, w, arma::fill::randu);
-    std::cout << "Generating matrix with " << matr.size() << "random elements \n";
+    std::cout << "Generating matrix with " << matr.size() << " random elements \n";
     return matr;
 }
 
