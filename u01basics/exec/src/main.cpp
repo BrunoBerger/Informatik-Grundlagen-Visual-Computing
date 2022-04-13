87ca55d3c0433c9eac0f60db044bfb581498d21f
BrunoBerger <bruno.berger@stud.hshl.de>
2022-04-13 15:39:06 +0200

    aenderungen nach feeback

--- u01basics/exec/src/main.cpp
+++ u01basics/exec/src/main.cpp
@@ -9,7 +9,11 @@
 int main(int argc, char* argv[])
 {
     std::vector<int> args = getIntArgs(argc, argv);
-
+    if (args.size() < 2 ) {
+        std::cout << "Please supply atleast 2 int arguments\n";
+        return 1;
+    }
+    arma::arma_rng::set_seed_random();
     arma::mat matr1 = getRandomMat(args[0], args[1]);
     arma::mat matr2 = getRandomMat(args[1], args[0]);
     auto res = matr1 * matr2;
