8bca0de5cf1f9c4f0f6e4ad96cfbbf84695ed75d
BrunoBerger <bruno.berger@stud.hshl.de>
2022-05-18 01:41:37 +0200

    u3 abgabe

---
+++ u03heat/benchmark/original_no_O/include/LaplaceHeat.h
@@ -0,0 +1,28 @@
+#ifndef INCLUDE_LAPLACEHEAT_H_
+#define INCLUDE_LAPLACEHEAT_H_
+
+class LaplaceHeat {
+public:
+
+	int XSIZE;
+	int YSIZE;
+
+	double HEAT_MIN;
+	double HEAT_MAX;
+
+	int STEPS;
+	int NITER;
+
+	LaplaceHeat(int xsize, int ysize, double heat_min, double heat_max, int steps, int niter);
+	~LaplaceHeat();
+
+	double* h; // Finite difference array.
+	double* h_new; // Finite difference array (new values).
+
+	void update();
+	void heatSource();
+	unsigned long checksum();
+
+};
+
+#endif /* INCLUDE_LAPLACEHEAT_H_ */
