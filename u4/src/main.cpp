e07eeeeab6f13f6a6a93cf4337e944eb4cd0b70d
Bruno Berger <brunoberger.dev@gmail.com>
2022-06-01 19:30:23 +0200

    matrix multiplication + basic angle calc

--- u4/src/main.cpp
+++ u4/src/main.cpp
@@ -1,4 +1,5 @@
 
+#include <array>
 #include <iostream>
 #include <math.h>
 
@@ -14,48 +15,92 @@ class Point3D {
             e2 = _e2;
             e3 = _e3;
         }
-        auto distance(Point3D other) {
+        float distance(Point3D<T> other) {
             auto x = other.e1 - e1;
             auto y = other.e2 - e2;
             auto z = other.e3 - e3;
             return sqrt(x*x + y*y + z*z);
         }
-        auto dotProduct(Point3D other) {
+        float magnitude() {
+            Point3D<T> nullVec(0,0,0);
+            return this->distance(nullVec);
+        }
+        float dotProduct(Point3D other) {
             return e1*other.e1 + e2*other.e2 + e3*other.e3;
         }
-
+        float angle(Point3D other) {
+            auto temp = this->dotProduct(other) / (this->magnitude() * other.magnitude());
+            return acos(temp) * 180 / M_PI;
+        }
 
         friend std::ostream& operator << (std::ostream &os , const Point3D& v ) {
             return os << "(" << v.e1 << "|" << v.e2 << "|" << v.e3 << ")";
         }
 };
 
-
 template <typename T>
 class Vector3D : public Point3D<T> {
-     using Point3D<T>::Point3D;
+    using Point3D<T>::Point3D;
+
+    public:
+        std::array<T, 4> multiplyWithMatr(float mat[4][4]) {
+            std::array<T, 4> res;
+            for (int i=0; i<4; i++) {
+                res[i] = 
+                    mat[i][0] * this->e1 +
+                    mat[i][1] * this->e2 + 
+                    mat[i][2] * this->e3 +
+                    mat[i][3] * 1;
+            }
+            return res;
+        };
 };
+
 
 
 enum Direction {REJECTED, RIGHT, LEFT};
 
-// either add converstion or give pos and wp the same type
-template<typename T1, typename T2, typename T3, typename T4>
-Direction testWayPoint(Point3D<T1> dronePosition, Vector3D<T2> droneForward, Vector3D<T3> droneUp, Point3D<T4> waypoint){
-    if (dronePosition.distance(waypoint) >= 10)
+template<typename T1, typename T2>
+Direction testWayPoint(Point3D<T1> dronePosition, Vector3D<T1> droneForward, Vector3D<T2> droneUp, Point3D<T1> waypoint){
+    float distance = dronePosition.distance(waypoint);
+    float angle = droneForward.angle(waypoint);
+
+    std::cout << "distance: " << distance << std::endl;
+    std::cout << "angle: " << angle << std::endl;
+
+    if (distance >= 10)
         return REJECTED;
-    
+    if (abs(angle) > 60)
+        return REJECTED;
+
     return RIGHT;
 }
 
-int main() {
-    Point3D<float> dPos(0, 0, 0);
-    Vector3D<int> dForward(1, 1, 1);
-    Vector3D<int> dUp(0, 1, 0);
-    Point3D<float> wp(4, 0, 0);
-    std::cout << dPos << dForward << dUp << wp << std::endl;
 
+int main() {
+    // Testing testWayPoint function
+    Point3D<float> dPos(0, 0, 0);
+    Vector3D<float> dForward(0, 1, 0);
+    Vector3D<int> dUp(0, 1, 0);
+    Point3D<float> wp(0, 3, 4);
+    std::cout << "Input vecs: "<< dPos << dForward << dUp << wp << std::endl;
    
     int x = testWayPoint(dPos, dForward, dUp, wp);
-    std::cout << x << std::endl;
+    std::cout << "Waypoint acceptance: " << x << std::endl;
+
+
+    // Testing matrix multiplication
+    float mat[4][4];
+    mat[0][0] = 1; mat[0][1] = 2; mat[0][2] = 3; mat[0][3] = 4;
+    mat[1][0] = 1; mat[1][1] = 2; mat[1][2] = 1; mat[1][3] = 1;
+    mat[2][0] = 1; mat[2][1] = 1; mat[2][2] = 2; mat[2][3] = 1;
+    mat[3][0] = 1; mat[3][1] = 1; mat[3][2] = 1; mat[3][3] = 2;
+
+    Vector3D<int> matTestVec(2, 3, 4);
+    auto res = matTestVec.multiplyWithMatr(mat);
+    for (auto i : res) {
+        std::cout << i << std::endl;
+    }
+    // For comparison: 
+    // https://matrixcalc.org/de/#%7B%7B1,2,3,4%7D,%7B1,2,1,1%7D,%7B1,1,2,1%7D,%7B1,1,1,2%7D%7D*%7B%7B2%7D,%7B3%7D,%7B4%7D,%7B1%7D%7D
 }
